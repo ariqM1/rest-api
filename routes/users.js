15c32ceff0f5f455db16cd9cfe88d4f8e1334992
Ariq Mukul <you@example.com>
2022-06-04 16:47:31 -0600

    rest api

---
+++ routes/users.js
@@ -0,0 +1,21 @@
+import express from 'express';
+
+import { createUser, getUser, getUsers, deleteUser, updateUser } from '../controllers/users.js';
+
+const router = express.Router();
+
+//all routes in here begin with /users
+
+router.get('/', getUsers);
+
+router.post('/', createUser);
+
+router.get('/:id', getUser);
+
+router.delete('/:id', deleteUser);
+
+router.patch('/:id', updateUser);
+
+
+export default router;
+
